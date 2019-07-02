b12f4aae0d53a7e07280261a13ae629a4971e4cb
gaurav singh <31446671+singhgaurav2323@users.noreply.github.com>
2019-07-02 18:13:09 +0530

    Add files via upload
    support vector regression

---
+++ svr_self.py
@@ -0,0 +1,40 @@
+#svr regression
+
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Position_Salaries.csv')
+X = dataset.iloc[:, 1:2].values
+y = dataset.iloc[:, -1:].values
+
+# Splitting the dataset into the Training set and Test set
+"""from sklearn.cross_validation import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)"""
+
+# Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+sc_y = StandardScaler()
+X = sc_X.fit_transform(X)
+y = sc_y.fit_transform(y)
+
+#svr regression model                 note: svr model donot do feture scaling with its own as it is less known class
+from sklearn.svm import SVR
+regressor=SVR(kernel='rbf',gamma='auto')
+regressor.fit(X,y)
+
+# Predicting a new result
+y_pred=sc_y.inverse_transform(regressor.predict(sc_X.fit_transform(np.array([[6.5]]))))
+
+# Visualising the Regression results
+plt.scatter(X, y, color = 'red',label='actual stat')
+plt.plot(X, regressor.predict(X), color = 'blue',label='predicted stat')
+plt.legend()
+plt.title('Truth or Bluff (SVR Model)')
+plt.xlabel('Position level')
+plt.ylabel('Salary')
+plt.show()
